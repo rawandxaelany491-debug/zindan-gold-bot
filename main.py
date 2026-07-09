9cc495e8960906d43c125c3a0cac41ce439eda3f
rawandxaelany491-debug <rawandxaelany491@gmail.com>
2026-07-09 08:30:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -125,3 +125,29 @@ await update.message.reply_text(
     "📊 شیکاری AI:\n\n"
     f"{analysis}"
 )
+def main():
+
+    application = (
+        Application.builder()
+        .token(BOT_TOKEN)
+        .build()
+    )
+
+    application.add_handler(
+        CommandHandler("start", start)
+    )
+
+    application.add_handler(
+        MessageHandler(
+            filters.PHOTO,
+            handle_photo,
+        )
+    )
+
+    logger.info("Gold Bot Started...")
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
