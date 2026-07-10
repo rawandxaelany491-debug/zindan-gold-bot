3d0e337e084d94fae6c4ed815b43b924b3c04581
rawandxaelany491-debug <rawandxaelany491@gmail.com>
2026-07-11 00:24:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,15 @@ from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
-    ContextTypes,
-    MessageHandler,
     CallbackQueryHandler,
-    filters,
+    ContextTypes,
 )
 
 from config import BOT_TOKEN, logger
 from handlers import (
-    button,
-    handle_message,
-    help_command,
     start,
-    unknown,
+    help_command,
+    button_handler,
 )
 
 
@@ -57,28 +53,12 @@ def main():
         CommandHandler("help", help_command)
     )
 
-    # Buttons
+    # Inline Buttons
     application.add_handler(
-        CallbackQueryHandler(button)
+        CallbackQueryHandler(button_handler)
     )
 
-    # Text Messages
-    application.add_handler(
-        MessageHandler(
-            filters.TEXT & ~filters.COMMAND,
-            handle_message,
-        )
-    )
-
-    # Unknown Commands
-    application.add_handler(
-        MessageHandler(
-            filters.COMMAND,
-            unknown,
-        )
-    )
-
-    # Error Handler
+    # Errors
     application.add_error_handler(
         error_handler
     )
