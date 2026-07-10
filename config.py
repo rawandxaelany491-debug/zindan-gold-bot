f888012c098d3abafc50a8ecd0bad128076895b4
rawandxaelany491-debug <rawandxaelany491@gmail.com>
2026-07-10 11:54:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 class Config:
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
     @classmethod
     def validate(cls):
         if not cls.TELEGRAM_BOT_TOKEN:
             raise ValueError("TELEGRAM_BOT_TOKEN is missing")
 
-        if not cls.OPENROUTER_API_KEY:
-            raise ValueError("OPENROUTER_API_KEY is missing")
+        if not cls.GROQ_API_KEY:
+            raise ValueError("GROQ_API_KEY is missing")
