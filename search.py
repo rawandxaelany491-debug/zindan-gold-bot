aa77f3eef895723fb408f8faf4e055689db247c0
rawandxaelany491-debug <rawandxaelany491@gmail.com>
2026-07-11 03:08:56 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,1 +1,10 @@
-ا
+from knowledge import SNRZ_DATA
+
+def search_snrz(question):
+    question = question.lower()
+
+    for key, value in SNRZ_DATA.items():
+        if key.lower() in question:
+            return value
+
+    return "ببورە، ئەم بابەتە لە SNRZ نەدۆزرایەوە."
