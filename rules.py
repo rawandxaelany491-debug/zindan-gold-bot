1b84c48dc96e7c74b0ddc947c4306291fae560fb
rawandxaelany491-debug <rawandxaelany491@gmail.com>
2026-07-12 22:54:52 +0300

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,1 +1,31 @@
-y
+SNRZ_RULES = {
+    "S": "Support",
+    "R": "Resistance",
+    "SBR": "Support Breakout to Resistance",
+    "RBS": "Resistance Breakout to Support",
+    "VS": "Valid Support",
+    "VR": "Valid Resistance",
+    "IVS": "Inversion Valid Support",
+    "IVR": "Inversion Valid Resistance",
+    "PO2": "Power Of Second Touch",
+    "SRR": "Support Breakout 2 Resistance",
+    "RSS": "Resistance Breakout 2 Support",
+    "GAP": "Gap Strategy",
+    "FALSE_BREAKOUT": "False Breakout Area",
+    "LIQUIDITY_SWEEP": "Liquidity Sweep",
+    "LIQUIDITY_RUN": "Liquidity Run"
+}
+
+BUY_SETUPS = ["VS", "IVR", "RBS", "SRR"]
+SELL_SETUPS = ["VR", "IVS", "SBR", "RSS"]
+
+TIMEFRAME_FLOW = [
+    "Weekly",
+    "Daily",
+    "4H",
+    "1H",
+    "30m",
+    "15m",
+    "5m",
+    "1m"
+]
